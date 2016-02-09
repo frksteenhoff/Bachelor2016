7a47391a1399914fc3e53883f9a56e94e7291c80
Henriette Steenhoff <henriette_steenhoff@outlook.dk>
2016-02-09 13:08:39 +0100

    Initial commit of project

---
+++ src/ReadFile.java
@@ -0,0 +1,12 @@
+/*
+ * Henriette Steenhoff, s134869
+ * Bachelor project 2016
+ * Algoritms for Allocation Procedures
+ */
+
+public class ReadFile {
+
+	public static void main(String[] args){
+		
+	}
+}
